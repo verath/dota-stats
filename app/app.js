5a9317208cbe45840baf632a12aaf2262e164cba
Peter Eliasson <warackta@gmail.com>
2014-08-04 16:20:01 +0200

    Name change for nodetime.

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 if(process.env.NODETIME_ACCOUNT_KEY) {
     require('nodetime').profile({
         accountKey: process.env.NODETIME_ACCOUNT_KEY,
-        appName: 'My Application Name' // optional
+        appName: 'dota-stats' // optional
     });
 }
 
