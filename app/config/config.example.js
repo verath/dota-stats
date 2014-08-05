5e97a5356550433e53ce213d5246cb4cbcb651a7
Peter Eliasson <warackta@gmail.com>
2014-08-05 16:30:33 +0200

    Updated example config file for backend.

--- app/config/config.example.js
+++ app/config/config.example.js
@@ -1,6 +1,8 @@
 module.exports = {
     'STEAM_API_KEY': '', // http://steamcommunity.com/dev/apikey
 
+    SITE_URL: 'http://127.0.0.1:3000/',
+
     'REDIS_PORT': null,
     'REDIS_HOST': null,
     'REDIS_OPTIONS': null
